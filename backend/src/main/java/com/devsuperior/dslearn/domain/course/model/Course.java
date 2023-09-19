5230fb728ee4639e845a5a82885e94eacff9a6aa
Pedro Oliveira <012pedropaulo@gmail.com>
2023-09-19 17:47:14 -0300

    Add course and data seeding of class course

---
+++ backend/src/main/java/com/devsuperior/dslearn/domain/course/model/Course.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslearn.domain.course.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity(name = "tb_course")
+@Table(name = "tb_course")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Lob
+    private String imgUri;
+    @Lob
+    private String imgGrayUri;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
